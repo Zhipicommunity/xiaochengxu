73620ae2f9dccbead197b5044e2e84916aa8d76c
Zhipicommunity <129773176+Zhipicommunity@users.noreply.github.com>
2023-04-06 22:48:58 +0800

    Update models.py

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -1,27 +1,41 @@
-from datetime import datetime
-
 from django.db import models
 
 
-# # Create your models here.
+# Create your models here.
 # class Counters(models.Model):
 #     id = models.AutoField
 #     count = models.IntegerField(max_length=11, default=0)
 #     createdAt = models.DateTimeField(default=datetime.now(), )
 #     updatedAt = models.DateTimeField(default=datetime.now(),)
-
+#
 #     def __str__(self):
 #         return self.title
-
+#
 #     class Meta:
-#         db_table = 'Counters'  # 数据库表名
-class UsersInfo(models.Model):
-    id=models.AutoField
-    username=models.CharField(max_length=64)
-    userpwd=models.IntegerField(max_length=11)
-    fabuneirong=models.CharField()
-    createdAt = models.DateTimeField(default=datetime.now(), )
-    updatedAt = models.DateTimeField(default=datetime.now(),)
+#         db_table = 'counter'  # 数据库表名
+# # class UsersInfo(models.Model):
+#     id=models.AutoField
+#     usersname=models.CharField(max_length=64)
+#     userspwd=models.IntegerField(max_length=11)
+#     neirong=models.CharField()
+#     createdAt = models.DateTimeField(default=datetime.now(), )
+#     updatedAt = models.DateTimeField(default=datetime.now(),)
+#
+#     class My_sql:
+#         db_data='usersinfo'
 
-    class My_sql:
-        db_data='usersinfo'
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    # 其他字段
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=100)
+    description = models.TextField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
