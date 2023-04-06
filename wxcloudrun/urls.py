c622aa8ed03fc2d6f02afaf994c3aaca7e0af450
Zhipicommunity <129773176+Zhipicommunity@users.noreply.github.com>
2023-04-06 22:49:39 +0800

    Update urls.py

--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -1,26 +1,9 @@
-"""wxcloudrun URL Configuration
+from django.urls import path
+from wxcloudrun.views import PetListCreateView
+from wxcloudrun.views import UserLoginView
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from wxcloudrun import views
-from django.conf.urls import url
-
-urlpatterns = (
-    # 计数器接口
-    url(r'^^api/count(/)?$', views.counter),
-
-    # 获取主页
-    url(r'(/)?$', views.index),
-)
+urlpatterns = [
+    path('api/', PetListCreateView.as_view(), name='pet_list_create'),
+    path('login/', UserLoginView.as_view(), name='user_login'),
+    # 其他 URL 路由
+]
