74d648c9cc2031cd06496e89515e38de15915092
Zhipicommunity <129773176+Zhipicommunity@users.noreply.github.com>
2023-04-06 23:34:17 +0800

    Update urls.py

--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -5,5 +5,6 @@ from wxcloudrun.views import UserLoginView
 urlpatterns = [
     path('api/', PetListCreateView.as_view(), name='pet_list_create'),
     path('login/', UserLoginView.as_view(), name='user_login'),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
     # 其他 URL 路由
 ]
